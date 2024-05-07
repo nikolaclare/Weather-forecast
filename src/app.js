ffe6073f61d2671cbb4abb13d50caac06e3b19a9
nikola ashton <132887065+nikolaclare@users.noreply.github.com>
2024-05-07 17:03:56 +1000

    added search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+function updateWeatherApp(response) {
+  let temperatureElement = document.querySelector("#weather-app-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "04aft6704ff52a4b6b603b4986c4oc2a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeatherApp);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+  let searchInputElement = document.querySelector("#search-form-input");
+  searchCity(searchInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Melbourne");
